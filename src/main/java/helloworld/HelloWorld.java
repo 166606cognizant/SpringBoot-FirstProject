af4c9627ae1aacedfa95fa38469dbc710eab2d86
Shashi <sjaiswal@uncc.edu>
2018-03-05 13:36:56 -0500

    My 1st project

---
+++ src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+
+@SpringBootApplication
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorld.class, args);
+
+	}
+
+}
